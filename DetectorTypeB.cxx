425d6a6de5699a0f5a7f2746efc62af086e66b8d
Ole Hansen <ole@jlab.org>
2015-01-27 01:00:50 -0500

    Implement DetectorTypeB::Analyze (linear fit)

--- DetectorTypeB.cxx
+++ DetectorTypeB.cxx
@@ -26,15 +26,33 @@ void DetectorTypeB::Clear()
 
 int DetectorTypeB::Analyze()
 {
-  // This detector type computes some basic statistics of the raw data
+  // This detector type performs a linear fit to the data
 
   typedef vector<double> vec_t;
-  if( !data.empty() ) {
-    double n = double(data.size());
+  if( data.size() >= 3 ) {
+    double S11 = 0, S12 = 0, S22 = 0, G1 = 0, G2 = 0;
     for( vec_t::size_type i = 0; i < data.size(); ++i ) {
-      double x = data[i];
-      // TODO
+      double x = double(i)-2.0;
+      double y = data[i];
+      S11 += 1.0;
+      S12 += x;
+      S22 += x*x;
+      G1  += y;
+      G2  += x*y;
     }
+    double D = 1.0 / (S11*S22 - S12*S12);
+    inter = (G1*S22 - G2*S12)*D;
+    slope = (G2*S11 - G1*S12)*D;
+    cov11 = S11*D;
+    cov22 = S22*D;
+    cov12 = -S12*D;
+    chi2 = 0;
+    for( vec_t::size_type i = 0; i < data.size(); ++i ) {
+      double x = double(i)-2.0;
+      double d = inter + slope*x;
+      chi2 += d*d;
+    }
+    ndof = double(data.size())-2.0;
   }
 
   return 0;
@@ -48,13 +66,13 @@ void DetectorTypeB::Print() const
 int DetectorTypeB::DefineVariables( bool do_remove )
 {
   VarDef_t defs[] = {
-    { "slope", "Slope",              &slope },
-    { "inter", "Intercept",          &inter },
-    { "cov11", "Error in slope",     &cov11 },
-    { "cov22", "Error in intercept", &cov22 },
-    { "cov12", "Correlation coeff",  &cov12 },
-    { "ndof",  "Degrees of freedom", &ndof },
-    { "chi2",  "Chi2",               &chi2 },
+    { "slope", "Slope",                &slope },
+    { "inter", "Intercept",            &inter },
+    { "cov11", "Error^2 in slope",     &cov11 },
+    { "cov22", "Error^2 in intercept", &cov22 },
+    { "cov12", "Correlation coeff",    &cov12 },
+    { "ndof",  "Degrees of freedom",   &ndof },
+    { "chi2",  "Chi2",                 &chi2 },
     { 0 }
   };
   DefineVarsFromList( defs, GetName().c_str(), gVars, do_remove );
