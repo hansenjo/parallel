f0089875de43300520d566ce2758957f4bad245d
Ole Hansen <ole@jlab.org>
2020-03-19 20:22:20 -0400

    Add Examples directory to project, revive ThreadTest, update documentation

--- Examples/ThreadTest.cxx
+++ Examples/ThreadTest.cxx
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <cstdlib>
 #include <unistd.h>
+#include <ctime>
 
 using namespace std;
 using namespace ThreadUtil;
@@ -17,9 +18,9 @@ template< typename Data_t>
 class AnalysisThread : public PoolWorkerThread<Data_t>
 {
 public:
-  AnalysisThread( WorkQueue<Data_t>& wq, WorkQueue<Data_t>& fq,
-		  WorkQueue<Data_t>& rq )
-    : PoolWorkerThread<Data_t>(wq,fq,rq), fSeed(pthread_self())
+  AnalysisThread( WorkQueue<Data_t>& wq, WorkQueue<Data_t>& rq,
+                  const void* /* cfg */ )
+    : PoolWorkerThread<Data_t>(wq,rq), fSeed(time(0))
   {
     srand(fSeed);
   }
@@ -27,11 +28,12 @@ protected:
   virtual void run()
   {
     while( Data_t* data = this->fWorkQueue.next() ) {
-      // pthread_mutex_lock(&console_mutex);
-      // cout << "Thread " << pthread_self()
-      // 	   << ", data = " << setfill('0') << setw(5) << *data << endl;
-      // pthread_mutex_unlock(&console_mutex);
+//      pthread_mutex_lock(&console_mutex);
+//      cout << "Thread " << pthread_self()
+//           << ", data = " << setw(5) << *data << endl;
+//      pthread_mutex_unlock(&console_mutex);
       usleep((unsigned int)((float)rand_r(&fSeed)/(float)RAND_MAX*20000));
+      *data *= -1;
       this->fResultQueue.add(data);
     }
   }
@@ -43,47 +45,56 @@ template <typename Pool_t, typename Data_t>
 class OutputThread : public Thread
 {
 public:
-  OutputThread( Pool_t& pool ) : fPool(pool) {}
+  explicit OutputThread( Pool_t& pool, WorkQueue<Data_t>& freeQueue )
+          : fPool(pool), fFreeQueue(freeQueue) {}
 
 protected:
   virtual void run()
   {
     while( Data_t* data = fPool.nextResult() ) {
-      cout << "data = " << setfill('0') << setw(5) << *data << endl;
-      fPool.addFreeData(data);
+      cout << "data = " << setw(5) << *data << endl;
+      fFreeQueue.add(data);
     }
   }
 private:
   Pool_t& fPool;
+  WorkQueue<Data_t>& fFreeQueue;
 };
 
 
 int main( int /* argc */, char** /* argv */ )
 {
-  const size_t NDATA = 64;
+  const size_t NTHREADS = 16;
 
   typedef int thread_data_t;
   typedef ThreadPool<AnalysisThread,thread_data_t> thread_pool_t;
 
-  thread_pool_t pool(16);
-  OutputThread<thread_pool_t,thread_data_t> outp(pool);
+  // Set up an array of reusable context buffers and add them to
+  // the free queue
+  thread_data_t data[NTHREADS];
+  WorkQueue<thread_data_t> freeQueue;
+  for(int& item : data) {
+    freeQueue.add(&item);
+  }
+  // Set up the pool of worker threads
+  thread_pool_t pool(NTHREADS);
+  // Set up and start the output queue. It takes processed items from
+  // the pool's result queue, prints them, and puts them back into the
+  // free queue
+  OutputThread<thread_pool_t,thread_data_t> outp(pool, freeQueue);
   outp.start();
 
-  // Set up a pool of reusable context buffers
-  thread_data_t data[NDATA];
-  for( size_t i = 0; i < NDATA; ++i ) {
-    pool.addFreeData(&data[i]);
-  }
-
   // Add work
-  for( size_t i = 0; i < 10000; ++i ) {
-    thread_data_t* datap = pool.nextFree();
+  for( size_t i = 0; i < 1000; ++i ) {
+    thread_data_t* datap = freeQueue.next();
     *datap = i;
     pool.Process(datap);
+    // Note: Process() fills the pool's result queue and hence triggers
+    // action in the output queue
   }
 
   pool.finish();
-  pool.addResult(0);
+  pool.GetResultQueue().add(0);
   outp.join();
 
   return 0;
