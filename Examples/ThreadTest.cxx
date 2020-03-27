c2490e93579cf62fc35ce79df814b631c687ae56
Ole Hansen <ole@jlab.org>
2020-03-26 22:46:37 -0400

    Adapt ThreadTest to redesigned ThreadPool class

--- Examples/ThreadTest.cxx
+++ Examples/ThreadTest.cxx
@@ -11,22 +11,17 @@ using namespace ThreadUtil;
 // stdout is a shared resource, so protect it with a mutex
 mutex console_mutex;
 
-// Thanks to the reusable thread class implementing threads is
-// simple and free of pthread api usage.
-template< typename Data_t>
-class AnalysisThread : public PoolWorkerThread<Data_t>
+template <typename Data_t>
+class AnalysisWorker
 {
 public:
-  AnalysisThread( WorkQueue<Data_t>& wq, WorkQueue<Data_t>& rq,
-                  const void* /* cfg */ )
-    : PoolWorkerThread<Data_t>(wq,rq), fSeed(time(0))
+  AnalysisWorker() : fSeed( time( 0))
   {
     srand(fSeed);
   }
-protected:
-  virtual void run()
+  void run( ThreadPool<Data_t>* pool )
   {
-    while( Data_t* data = this->fWorkQueue.next() ) {
+    while( Data_t* data = pool->GetWorkQueue().next() ) {
 #ifdef DEBUG
       console_mutex.lock();
       cout << "Thread " << pthread_self()
@@ -35,24 +30,22 @@ protected:
 #endif
       usleep((unsigned int)((float)rand_r(&fSeed)/(float)RAND_MAX*20000));
       *data *= -10;
-      this->fResultQueue.add(data);
+      pool->GetResultQueue().add(data);
     }
   }
 private:
   unsigned int fSeed;
 };
 
-template <typename Pool_t, typename Data_t>
-class OutputThread : public Thread
+template <typename Data_t>
+class OutputWorker
 {
 public:
-  OutputThread( Pool_t& pool, WorkQueue<Data_t>& freeQueue )
-    : fPool(pool), fFreeQueue(freeQueue) {}
+  explicit OutputWorker( WorkQueue<Data_t>& freeQueue ) : fFreeQueue( freeQueue) {}
 
-protected:
-  virtual void run()
+  void run( ThreadPool<Data_t>* pool )
   {
-    while( Data_t* data = fPool.GetResultQueue().next() ) {
+    while( Data_t* data = pool->GetResultQueue().next() ) {
       console_mutex.lock();
       cout << "data = " << setw(5) << *data << endl << flush;
       console_mutex.unlock();
@@ -64,7 +57,6 @@ protected:
     console_mutex.unlock();
   }
 private:
-  Pool_t& fPool;
   WorkQueue<Data_t>& fFreeQueue;
 };
 
@@ -74,7 +66,6 @@ int main( int /* argc */, char** /* argv */ )
   const size_t NTHREADS = 16;
 
   using thread_data_t = int;
-  using thread_pool_t = ThreadPool<AnalysisThread,thread_data_t>;
 
   // Set up an array of reusable context buffers and add them to
   // the free queue
@@ -83,13 +74,14 @@ int main( int /* argc */, char** /* argv */ )
   for(int& item : data) {
     freeQueue.add(&item);
   }
+  AnalysisWorker<thread_data_t> analysisWorker;
   // Set up the pool of worker threads
-  thread_pool_t pool(NTHREADS);
+  ThreadPool pool(NTHREADS,analysisWorker);
   // Set up and start the output queue. It takes processed items from
   // the pool's result queue, prints them, and puts them back into the
   // free queue
-  OutputThread<thread_pool_t,thread_data_t> outp(pool, freeQueue);
-  outp.start();
+  OutputWorker<thread_data_t> outputWorker( freeQueue);
+  std::thread outp(&OutputWorker<thread_data_t>::run, outputWorker, &pool);
 
   // Add work
   for( size_t i = 0; i < 10000; ++i ) {
