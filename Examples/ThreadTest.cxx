04f3767b14750433c4bea151c5700e930bdf4d78
Ole Hansen <ole@jlab.org>
2020-11-18 01:27:37 -0500

    ThreadTest: Avoid random number baggage in AnalysisWorker
    AnalysisWorker could now actually be a free function instead of a class,
    but let's leave it as it as for illustration. Also, while this is
    cleaner code, it doesn't actually save much memory since there is still
    one heavy generator object per thread. Not that that's a real problem,
    of course, it's small enough.
    
    Also, eliminate the unused instance of AnalysisWorker in the main
    program.

--- Examples/ThreadTest.cxx
+++ Examples/ThreadTest.cxx
@@ -3,35 +3,40 @@
 #include <iomanip>
 #include <chrono>
 #include <random>
+#include <thread>
 
 using namespace std;
 using namespace ThreadUtil;
 
-// stdout is a shared resource, so protect it with a mutex
 static mutex console_mutex;
 static random_device rd;
+
+using namespace std;
+
+// Thread-safe random number generator
+int intRand(int min, int max) {
+  thread_local minstd_rand generator(rd());
+  uniform_int_distribution<int> distribution(min, max);
+  return distribution(generator);
+}
 
 template<typename Data_t>
 class AnalysisWorker {
 public:
-  AnalysisWorker() : fGen(rd()), fRand(1, 20) {}
   void run( QueuingThreadPool<Data_t>* pool ) {
-    while( std::unique_ptr<Data_t> data = pool->pop_work() ) {
+    while( std::unique_ptr<Data_t> datap = pool->pop_work() ) {
 #ifdef DEBUG
       console_mutex.lock();
       cout << "Thread " << pthread_self()
            << ", data = " << setw(5) << *data << endl << flush;
       console_mutex.unlock();
 #endif
-      int ms = fRand(fGen);
+      int ms = intRand(1,20);
       std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-      *data *= -10;
-      pool->push_result(std::move(data));
+      *datap *= -10;
+      pool->push_result(std::move(datap));
     }
   }
-private:
-  minstd_rand fGen;
-  uniform_int_distribution<int> fRand;
 };
 
 template<typename Data_t>
@@ -58,24 +63,22 @@ private:
 };
 
 
-int main( int /* argc */, char** /* argv */ )
+int main( int /* argc */, const char*[] /* argv */ )
 {
   const size_t NTHREADS = 16;
 
+  // Data type to be processed in the threads
   using thread_data_t = int;
 
-  // Set up an array of reusable context buffers and add them to
-  // the free queue
+  // Allocate data objects and add them to the free queue
   WorkQueue<thread_data_t> freeQueue;
   for( size_t i=0; i<NTHREADS; ++i ) {
     std::unique_ptr<thread_data_t> item(new thread_data_t);
     freeQueue.push(std::move(item));
   }
-  AnalysisWorker<thread_data_t> analysisWorker;
   // Set up the pool of worker threads
-  QueuingThreadPool<thread_data_t> pool(NTHREADS, analysisWorker);
-  // With template argument deduction, this works too, although it's less clear
-  //QueuingThreadPool pool(NTHREADS, analysisWorker);
+  QueuingThreadPool pool(NTHREADS, AnalysisWorker<thread_data_t>());
+
   // Set up and start the output queue. It takes processed items from
   // the pool's result queue, prints them, and puts them back into the
   // free queue
@@ -87,8 +90,8 @@ int main( int /* argc */, char** /* argv */ )
     std::unique_ptr<thread_data_t> datap = freeQueue.next();
     *datap = i;
     pool.push_work(std::move(datap));
-    // Note: push_work() fills the pool's result queue and hence triggers
-    // action in the output queue
+    // push_work() wakes up the analysis workers. The workers then fill the
+    // pool's result queue. This in turn triggers action in the output queue.
   }
 
   pool.finish();
