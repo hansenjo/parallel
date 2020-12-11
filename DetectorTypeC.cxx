d4ec8512dd291cc182e770b8151624ba6a5531dd
Ole Hansen <ole@jlab.org>
2020-12-11 10:37:24 -0500

    Add DetectorTypeC, implementing an example of a time-consuming algorithm

---
+++ DetectorTypeC.cxx
@@ -0,0 +1,124 @@
+// Parallel Podd DetectorTypeC
+//
+// Simple "detector" class, doing computation of pi as an example
+// of a time-consuming task
+
+#include "Podd.h"
+#include "DetectorTypeC.h"
+#include "Decoder.h"
+#include <cstdlib>
+#include <algorithm>
+
+using namespace std;
+
+DetectorTypeC::DetectorTypeC( const string& name, int imod )
+  : Detector(name, imod), m_ndig(0), m_last8(0)
+{
+  type = "C";
+}
+
+DetectorTypeC::~DetectorTypeC()
+{
+ DetectorTypeC::DefineVariables( kRemove );
+}
+
+void DetectorTypeC::Clear()
+{
+  Detector::Clear();
+  m_result.clear();
+  m_ndig  = 0;
+  m_last8 = 0;
+}
+
+unique_ptr<Detector> DetectorTypeC::Clone() const
+{
+  return make_unique<DetectorTypeC>(*this);
+}
+
+
+int DetectorTypeC::Analyze()
+{
+  // This detector type computes n digits of pi
+  //
+  // Implements the algorithm from
+  // Rabinowitz and Wagon, "A spigot algorithm for the digits of Pi",
+  // American Mathematical Monthly, 102 (3), 195–203 (March 1995),
+  // doi:10.2307/2975006.
+  //
+  // Note: There appears to be a bug in this code that sometimes gets the
+  // last digit wrong. Compare the results for n = 50 and n = 51 for example.
+  // We ignore this problem here.
+
+  int n = 0;
+  if( !data.empty() )
+    n = int(data[0]);
+  if( n < 1 )
+    n = 10;
+
+  int N = (10*n)/3;
+
+  m_a.resize(N);
+  m_a.assign(N,2);
+  m_result.reserve(n+1);
+
+  auto times_ten = [](int& a) { a *= 10; };
+
+  int last_digit = -1;
+  int nines = 0;
+  bool dot = true;
+  for( int i = 0; i < n; i++ ) {
+    for_each( ALL(m_a), times_ten );
+    for( int j = N-1; j > 0; j-- ) {
+      div_t d = div(m_a[j],2*j+1);
+      m_a[j] = d.rem;
+      m_a[j-1] += d.quot*j;
+    }
+    int Q = m_a[0]/10;
+    m_a[0] -= 10*Q;
+    if( Q < 9 ) {
+      if( last_digit >= 0 )
+        m_result += char('0'+last_digit);
+      if( dot && last_digit >= 0 ) {
+        dot = false;
+        m_result += '.';
+      }
+      for( int j = 0; j < nines; j++ )
+        m_result += '9';
+      nines = 0;
+      last_digit = Q;
+    } else if ( Q == 9 ) {
+      nines ++;
+    } else if ( Q == 10 ) {
+      if( last_digit >= 0 )
+        m_result += char('1'+last_digit);
+      if( dot && last_digit >= 0 ) {
+        dot = false;
+        m_result += '.';
+      }
+      for( int j = 0; j < nines; j++ )
+        m_result += '0';
+      nines = 0;
+      last_digit = 0;
+    }
+
+  }
+  if( last_digit >= 0 )
+    m_result += char('0'+last_digit);
+
+  m_ndig = n;
+  return 0;
+}
+
+void DetectorTypeC::Print() const
+{
+  Detector::Print();
+}
+
+int DetectorTypeC::DefineVariables( bool do_remove )
+{
+  const vector<VarDef_t> defs = {
+          {"nval",  "Number of digits computed",  &m_ndig},
+          {"last8", "Value of last 8 digits",     &m_last8},
+  };
+  return DefineVarsFromList(defs, GetName(), fVars, do_remove);
+}
---
+++ DetectorTypeC.h
@@ -0,0 +1,34 @@
+// Simple detector example class
+//
+// This detector type computes N digits of pi, where N is taken from
+// the raw data input file
+
+#ifndef PPODD_DETECTORC
+#define PPODD_DETECTORC
+
+#include "Detector.h"
+#include <string>
+#include <vector>
+
+class Decoder;
+
+class DetectorTypeC : public Detector {
+public:
+  DetectorTypeC( const std::string& name, int imod );
+  virtual ~DetectorTypeC();
+
+  virtual void Clear();
+  [[nodiscard]] virtual std::unique_ptr<Detector> Clone() const;
+  virtual int  Analyze();
+  virtual void Print() const;
+
+protected:
+  std::vector<int> m_a;       // Workspace
+  std::string      m_result;  // Result as string representation of a decimal number
+  double           m_ndig;    // Number of digits computed (taken from raw data)
+  double           m_last8;   // Last 8 digits of result (for illustration)
+
+  virtual int  DefineVariables( bool remove );
+};
+
+#endif
