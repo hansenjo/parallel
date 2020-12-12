66dcc4d7188385c794e217e8d1f74992fef0ab43
Ole Hansen <ole@jlab.org>
2020-12-11 22:21:22 -0500

    Collect timing statistics in main program
    Also, refactor command-line parser & class organization.

--- Util.h
+++ Util.h
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <string>
+#include <ctime>
 
 //___________________________________________________________________________
 template< typename Container>
@@ -26,6 +27,24 @@ inline void CopyContainer( const Container& from, Container& to )
   to.clear();
   CopyObject<Container> copy(to);
   std::for_each( from.begin(), from.end(), copy );
+}
+
+//___________________________________________________________________________
+/**
+ * @fn timespec_diff(struct timespec *, struct timespec *, struct timespec *)
+ * @brief Compute the diff of two timespecs, that is a - b = result.
+ * @param a the minuend
+ * @param b the subtrahend
+ * @param result a - b
+ */
+static inline void timespec_diff(struct timespec *a, struct timespec *b,
+                                 struct timespec *result) {
+  result->tv_sec  = a->tv_sec  - b->tv_sec;
+  result->tv_nsec = a->tv_nsec - b->tv_nsec;
+  if (result->tv_nsec < 0) {
+    --result->tv_sec;
+    result->tv_nsec += 1000000000L;
+  }
 }
 
 //___________________________________________________________________________
